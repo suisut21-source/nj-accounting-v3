49e3de32fbc738a10e2b66f867009cb2444f3cc2
suisut21-source <suisut21@gmail.com>
2026-08-30 18:27:27 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="th">
       <body>
-        <div className="flex min-h-screen bg-[#F8FAFC] font-sans text-slate-800">
+        <div className="flex flex-col md:flex-row min-h-screen bg-[#F8FAFC] font-sans text-slate-800">
           <Sidebar />
           <main className="flex-1 p-6 overflow-y-auto">
             {children}
